870bca48ba3bf661fd4d53253dedfbcb7f1f249c
Bruno Scheufler <bszerofive@gmail.com>
2017-03-25 22:29:04 +0100

    Added FileSelection classes
    Added example import function for single files to FXMLDocumentController
    Added MultipleFileSection and SingleFileSelection class for an easy
    access to file selection functions with predefined extension filters for
    mp3 and wav

--- src/rainbowplayer/FXMLDocumentController.java
+++ src/rainbowplayer/FXMLDocumentController.java
@@ -6,23 +6,30 @@
 package rainbowplayer;
 
 import java.io.File;
+import java.io.IOException;
 import rainbowplayer.Classes.Title;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Optional;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.control.ButtonBar.ButtonData;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.Label;
 import javafx.scene.media.Media;
 import javafx.scene.media.MediaPlayer;
 import rainbowplayer.Classes.Duration;
 import rainbowplayer.Core.SongPlayer;
 import rainbowplayer.db.Database;
-import rainbowplayer.db.TrackFetcher;
 import rainbowplayer.db.TrackInsertion;
-import rainbowplayer.io.TrackImport;
+import rainbowplayer.io.SingleFileSelection;
 
 public class FXMLDocumentController implements Initializable {
     
@@ -86,46 +93,86 @@ public class FXMLDocumentController implements Initializable {
     
     @FXML
     private void handleImportButtonAction(ActionEvent event){
-        TrackImport tImport = new TrackImport();
-        switch(tImport.openSingleFileChooser()){
-            case "success":
-                File trackFile = tImport.getFile();
-                if(trackFile != null){
-                        //insert title into db
-                        Media tMedia = new Media(trackFile.toURI().toString());
-                        MediaPlayer mediaPlayer = new MediaPlayer(tMedia);
-                        mediaPlayer.setOnReady(()->{
-                            Title track = new Title(trackFile.getPath(),tMedia.getMetadata().get("title").toString(),tMedia.getMetadata().get("artist").toString());
-                        
-                            TrackInsertion tIns = new TrackInsertion();
-                            if(!tIns.insertTrack(track)){
-                              //could not insert track into db
-                            }
-                            
-                            //Feedback > returns inserted track
-                            TrackFetcher tFetch = new TrackFetcher();
-                            if(!tFetch.retrieveTrack(tIns.getTrackId())){
-                              //could not retrieve track from db
-                            }
+        Alert alert = new Alert(AlertType.INFORMATION);
+        alert.setTitle("Choose an import mode");
+        alert.setHeaderText("Choose an import mode");
+        alert.setContentText("You can either import a single file or import multiple files.");
 
-                            Title returnedTrack = tFetch.getTrack();
-                            System.out.println(returnedTrack.getFormattedTitle());
-                        
-                        });
-                      
-                }
-            break;
-            case "invalid":
-                    if(tImport.getFile() != null){
-                        System.out.println(tImport.getExtension(tImport.getFile()));
-                    }else{
-                        System.out.println("null");
-                    }
-                    System.out.println("Invalid file format. Supported file types are: .mp3 .wav");
-            break;
-            case "cancelled":
-                    System.out.println("No file selected.");
-            break;
+        ButtonType button_imp_singleFile = new ButtonType("Import Single File");
+        ButtonType button_imp_multiFile = new ButtonType("Import Multiple Files");
+        ButtonType button_cancel = new ButtonType("Cancel", ButtonData.CANCEL_CLOSE);
+
+        alert.getButtonTypes().setAll(button_imp_singleFile, button_imp_multiFile, button_cancel);
+        
+        Optional<ButtonType> result = alert.showAndWait();
+        if (result.get() == button_imp_singleFile){
+            
+            
+            // single file import
+            SingleFileSelection sfs = new SingleFileSelection();
+            switch(sfs.selectSingleFile()){
+                case "success":
+                    
+                    File fileToImport = sfs.getFile();
+                    
+                    Media tMedia = new Media(fileToImport.toURI().toString());
+                    MediaPlayer mediaPlayer = new MediaPlayer(tMedia);
+                    
+                    tMedia.setOnError(()->{
+                        Alert alert_error = new Alert(AlertType.ERROR);
+                        alert_error.setTitle("Error");
+                        alert_error.setHeaderText(null);
+                        alert_error.setContentText("Could not parse file: " + fileToImport.getName() + ".");
+                        alert_error.showAndWait(); 
+                    });
+                    
+                    mediaPlayer.setOnReady(() -> {
+                        String filePath = "";
+                        try {
+                            filePath = fileToImport.getCanonicalPath();
+                        } catch (IOException ex) {
+                            Logger.getLogger(FXMLDocumentController.class.getName()).log(Level.SEVERE, null, ex);
+                        }
+                        //TODO add more metadata
+                        String mediaTitle = tMedia.getMetadata().get("title").toString();
+                        String mediaArtist = tMedia.getMetadata().get("artist").toString();
+                        Title t = new Title(filePath,mediaTitle,mediaArtist);
+                        TrackInsertion tIns = new TrackInsertion();
+                        if(!tIns.insertTrack(t)){
+                            //insert failed
+                            Alert alert_error = new Alert(AlertType.ERROR);
+                            alert_error.setTitle("Error");
+                            alert_error.setHeaderText(null);
+                            alert_error.setContentText("Could not import file: " + fileToImport.getName() + ".");
+                            alert_error.showAndWait();
+                        }
+                        String trackId = tIns.getTrackId();
+                        Alert alert_success = new Alert(AlertType.CONFIRMATION);
+                        alert_success.setTitle("Success");
+                        alert_success.setHeaderText(null);
+                        alert_success.setContentText("The track was successfully imported!");
+                        alert_success.showAndWait();
+                        //TODO rerender UI
+            });
+                    
+                break;
+                case "invalid_format":
+                    Alert alert_invalid_format = new Alert(AlertType.ERROR);
+                    alert_invalid_format.setTitle("Invalid File Type");
+                    alert_invalid_format.setHeaderText(null);
+                    alert_invalid_format.setContentText("Please select a valid media file type. The player supports mp3 and wav files.");
+                    alert_invalid_format.showAndWait();
+                break;
+                case "no_selection":
+                    Alert alert_no_selection = new Alert(AlertType.WARNING);
+                    alert_no_selection.setTitle("Warning");
+                    alert_no_selection.setHeaderText(null);
+                    alert_no_selection.setContentText("Please select a file you want to import!");
+                    alert_no_selection.showAndWait();
+                break;
+            }
+        } else if (result.get() == button_imp_multiFile) {
+            // multiple file import
         }
     }
     

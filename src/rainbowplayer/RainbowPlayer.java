5f2406043eb09124256ce68351319bf9795089d6
maqnico <nm@liebigschule-giessen.de>
2017-06-02 18:05:56 +0200

    button actions
    added button methods

--- src/rainbowplayer/RainbowPlayer.java
+++ src/rainbowplayer/RainbowPlayer.java
@@ -9,19 +9,25 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 public class RainbowPlayer extends Application {
     
     @Override
     public void start(Stage stage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
+        stage.initStyle(StageStyle.UNIFIED); 
+        Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument_1.fxml"));
         
+    
         Scene scene = new Scene(root);
         
         stage.setScene(scene);
         stage.show();
         stage.setTitle("Rainbow Player");
+      
+        Font.loadFont(RainbowPlayer.class.getResource("fontawesome-webfont.ttf").toExternalForm(), 10);
     }
 
     /**

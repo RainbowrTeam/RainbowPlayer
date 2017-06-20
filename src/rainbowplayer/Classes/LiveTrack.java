5b38a2ce08fb0d9f0f2f7ac7f796387b36065e69
Tim W <tim@weiss.moe>
2017-06-20 22:32:29 +0200

    Linked UI with Backend
    it just werks

--- src/rainbowplayer/Classes/LiveTrack.java
+++ src/rainbowplayer/Classes/LiveTrack.java
@@ -15,18 +15,29 @@ public class LiveTrack extends Track {
     private int remainingSeconds;
     
     private Duration remainingDuration;
+    private Duration totalDuration;
     
     public LiveTrack(String path, String tit, String art) {
         super(path, tit, art);
         remainingDuration = new Duration();
+        totalDuration = new Duration();
+        // totalDuration.setTotalSeconds(duration);
     }
     
     public void setRemainingSeconds(int sec) {
         remainingDuration.setTotalSeconds(sec);
     }
     
+    public void setTotalSeconds(int sec) {
+        totalDuration.setTotalSeconds(sec);
+    }
+    
     public Duration getRemainingDuration(){
         return remainingDuration;
+    }
+    
+    public Duration getTotalDuration() {
+        return totalDuration;
     }
     
     public void setIsPaused(boolean paused) {
